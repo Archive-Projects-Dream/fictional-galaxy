346f8a094640e9d3eade217b84b3fc276b7b4bc7
Cepera <gundar.sereoja@yandex.ru>
2019-11-27 01:16:38 +0500

    Первые нароботки работы с PowerShell

--- form.designer.ps1
+++ form.designer.ps1
@@ -1,45 +1,28 @@
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
-
-$form = New-Object System.Windows.Forms.Form
-$form.Text = 'Data Entry Form'
-$form.Size = New-Object System.Drawing.Size(300,200)
-$form.StartPosition = 'CenterScreen'
-
-$OKButton = New-Object System.Windows.Forms.Button
-$OKButton.Location = New-Object System.Drawing.Point(75,120)
-$OKButton.Size = New-Object System.Drawing.Size(75,23)
-$OKButton.Text = 'OK'
-$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
-$form.AcceptButton = $OKButton
-$form.Controls.Add($OKButton)
-
-$CancelButton = New-Object System.Windows.Forms.Button
-$CancelButton.Location = New-Object System.Drawing.Point(150,120)
-$CancelButton.Size = New-Object System.Drawing.Size(75,23)
-$CancelButton.Text = 'Cancel'
-$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
-$form.CancelButton = $CancelButton
-$form.Controls.Add($CancelButton)
-
-$label = New-Object System.Windows.Forms.Label
-$label.Location = New-Object System.Drawing.Point(10,20)
-$label.Size = New-Object System.Drawing.Size(280,20)
-$label.Text = 'Please enter the information in the space below:'
-$form.Controls.Add($label)
-
-$textBox = New-Object System.Windows.Forms.TextBox
-$textBox.Location = New-Object System.Drawing.Point(10,40)
-$textBox.Size = New-Object System.Drawing.Size(260,20)
-$form.Controls.Add($textBox)
-
-$form.Topmost = $true
-
-$form.Add_Shown({$textBox.Select()})
-$result = $form.ShowDialog()
-
-if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+$Form1 = New-Object -TypeName System.Windows.Forms.Form
+[System.Windows.Forms.Button]$Button1 = $null
+function InitializeComponent
 {
-    $x = $textBox.Text
-    $x
+$Button1 = (New-Object -TypeName System.Windows.Forms.Button)
+$Form1.SuspendLayout()
+#
+#Button1
+#
+$Button1.Location = (New-Object -TypeName System.Drawing.Point -ArgumentList @([System.Int32]87,[System.Int32]112))
+$Button1.Name = [System.String]'Button1'
+$Button1.Size = (New-Object -TypeName System.Drawing.Size -ArgumentList @([System.Int32]75,[System.Int32]23))
+$Button1.TabIndex = [System.Int32]0
+$Button1.Text = [System.String]'Click me!'
+$Button1.UseCompatibleTextRendering = $true
+$Button1.UseVisualStyleBackColor = $true
+$Button1.add_Click($Button1_Click)
+#
+#Form1
+#
+$Form1.AcceptButton = $Button1
+$Form1.Controls.Add($Button1)
+$Form1.Text = [System.String]'Form1'
+$Form1.ResumeLayout($false)
+Add-Member -InputObject $Form1 -Name base -Value $base -MemberType NoteProperty
+Add-Member -InputObject $Form1 -Name Button1 -Value $Button1 -MemberType NoteProperty
 }
+. InitializeComponent
--- form.ps1
+++ form.ps1
@@ -1,3 +1,5 @@
+$Button1_Click = {
+}
 Add-Type -AssemblyName System.Windows.Forms
-.(Join-Path $PSScriptRoot 'form.designer.ps1')
+. (Join-Path $PSScriptRoot 'form.designer.ps1')
 $Form1.ShowDialog()
