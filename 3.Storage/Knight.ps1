25296311aa96c71de03d825fae05380423e5e822
Cepera <gundar.sereoja@yandex.ru>
2019-12-11 01:02:18 +0500

    Добавление исключений, и добавление проверки "пустых полей"

--- 3.Storage/Knight.ps1
+++ 3.Storage/Knight.ps1
@@ -14,20 +14,46 @@ Class Knight {
     [int]$Quantity = 0
     [System.Collections.ArrayList]$Array = @()
 
-    [int]AddStep([string]$Name, [string]$Storage) {
-        foreach($O in 0..$this.Quantity) {
-            if($this.Array[$O].storage -eq $null) {
+    [int]AddStep() {
+        $newStepId = $this.GetNewStepNumber()
+        $newStep = [KnightStorage]::new($newStepId, $null, $null)
+        $this.Array.Add($newStep)
+        return 0
+    }
+    [int]AddStep([string]$Name) {
+        foreach($O in 0..$this.Array.count) {
+            if($this.Array[$O].Name -eq $null) {
                 # Устанавливаем новый Id
                 $newStepId = $this.GetNewStepNumber()
                 # Вызываем конструктор и присваиваем значения из него к объекту внутри класса
-                $newStep = [KnightStorage]::new($newStepId, $Name, $Storage)    #region [Sun] #endregion
+                $newStep = [KnightStorage]::new($newStepId, $Name, $null)    #region [Sun] #endregion
                 # Добавляем к текушему массиву объект содержащий: (Id, Имя, и Груз); из конструктора.
                 $this.Array.Add($newStep)   #region [Fire] #endregion   
                 break
             }
+            elseif($this.Array[$O].Name -eq "") {
+                $this.Array[$O].Name = $Name
+                break
+            }
         }
         # Отладочное значение
-        return 555
+        return 1
+    }
+    [int]AddStep([string]$Name, [string]$Storage) {
+        foreach($O in 0..$this.Array.count) {
+            if($this.Array[$O].Name -eq $null) {
+                $newStepId = $this.GetNewStepNumber()
+                $newStep = [KnightStorage]::new($newStepId, $Name, $Storage)
+                $this.Array.Add($newStep)  
+                break
+            }
+            elseif($this.Array[$O].Name -eq "") {
+                $this.Array[$O].Name = $Name
+                $this.Array[$O].Storage = $Storage
+                break
+            }
+        }
+        return 2
     }
     # Метод отвечающий за показ значения ячейка внутри класса
     [int]GetStepName([int]$Id) {
@@ -38,16 +64,38 @@ Class Knight {
         return $this.Quantity += 1
     }
 }
+
 # Создаем объект
 $Knight = [Knight]::new()
 # Определяем через метод, что у нас будет в ячейках объекта
 $Knight.AddStep('Позолеченный сундук','Горсть золотых монет')
+$Knight.AddStep()
+$Knight.AddStep('Номер 2')
 $Knight.AddStep('Step Two','ScriptTwo.ps1')
+$Knight.AddStep('Тридцать три')
 
 # Смотрим что ~именно~ в массиве
 $Knight.Array
+# Проверка работы
 # $Knight.Array[1]
-$y = @()
+# $k = '[' + 1 + ']' + ': ""'
+# $k
+# $Knight.Array[1].name -eq ""
+# $k = '[' + 2 + ']' + ': ""'
+# $k
+# $Knight.Array[2].name -eq ""
+# $k = '[' + 6 + ']' + ': ""'
+# $k
+# $Knight.Array[6].name -eq ""
+# $k = '[' + 1 + ']' + ': $null'
+# $k
+# $Knight.Array[1].name -eq $null
+# $k = '[' + 2 + ']' + ': $null'
+# $k
+# $Knight.Array[2].name -eq $null
+# $k = '[' + 6 + ']' + ': $null'
+# $k
+# $Knight.Array[6].name -eq $null
 
 #region [Events]
 Function Main()
