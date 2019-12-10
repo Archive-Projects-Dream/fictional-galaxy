acba949691db51c5b22c5988916b0a59172257b3
Cepera <gundar.sereoja@yandex.ru>
2019-12-10 23:39:07 +0500

    Продолжая работы с хранилищами

--- 3.Storage/Index.ps1
+++ 3.Storage/Index.ps1
@@ -19,3 +19,39 @@ $Verter
 #endregion
 #region [Fire]
 #endregion
+
+#region Может быть и пригодиться (Наработки)
+#Class Load {
+#    Load($folder){
+#        $files = Get-ChildItem $folder 
+#        foreach ($file in $files) { 
+#            if ($file.length –gt 100MB) {
+#            }
+#        }
+#    }
+#} #endregion
+
+#region
+#Class Load2 {
+#    Load2(){
+#        # Сам не знаю зачем я это добавил, но берет из текстового документа случайную строку текста, и пишет её в консоль.
+#        $content = Get-Content C:\Users\user\Desktop\1.txt
+#        Get-Random -InputObject $content
+#    }
+#} #endregion
+
+#region Метод замены
+#Class Save : Kinght {
+#    #region [Nova]: Свойства Наследуемое
+#    # [int]$Id
+#    # [int]$Storage
+#    #endregion
+#    Load() {
+#    foreach ($Id in 1..10){
+#        if($Id -eq ""){
+#            sd
+#        }
+#    }
+#}
+#}
+#endregion
--- 3.Storage/Knight.ps1
+++ 3.Storage/Knight.ps1
@@ -15,15 +15,21 @@ Class Knight {
     [System.Collections.ArrayList]$Array = @()
 
     [int]AddStep([string]$Name, [string]$Storage) {
-        $newStepId = $this.GetNewStepNumber()
-        $newStep = [KnightStorage]::new($newStepId, $Name, $Storage)    #region [Sun] #endregion
-
-        # Добавляем к текушему массиву: Id, Имя, и Груз
-        $this.Array.Add($newStep)   #region [Fire] #endregion
-
-        return $newStepId
+        foreach($O in 0..$this.Quantity) {
+            if($this.Array[$O].storage -eq $null) {
+                # Устанавливаем новый Id
+                $newStepId = $this.GetNewStepNumber()
+                # Вызываем конструктор и присваиваем значения из него к объекту внутри класса
+                $newStep = [KnightStorage]::new($newStepId, $Name, $Storage)    #region [Sun] #endregion
+                # Добавляем к текушему массиву объект содержащий: (Id, Имя, и Груз); из конструктора.
+                $this.Array.Add($newStep)   #region [Fire] #endregion   
+                break
+            }
+        }
+        # Отладочное значение
+        return 555
     }
-    # Метод отвечающий за что то...
+    # Метод отвечающий за показ значения ячейка внутри класса
     [int]GetStepName([int]$Id) {
         return $this.Array[$Id-1]
     }
@@ -37,6 +43,28 @@ $Knight = [Knight]::new()
 # Определяем через метод, что у нас будет в ячейках объекта
 $Knight.AddStep('Позолеченный сундук','Горсть золотых монет')
 $Knight.AddStep('Step Two','ScriptTwo.ps1')
+
 # Смотрим что ~именно~ в массиве
 $Knight.Array
 # $Knight.Array[1]
+$y = @()
+
+#region [Events]
+Function Main()
+{
+    
+} #endregion
+#region [Main]
+# int main()
+# {
+#     MyStorage storage(10);
+#     for (int i = 0; i < storage.getCount(); i++) {
+#         storage.SetObject(i, new point());
+#     }
+#     for (int i = 0; i < storage.getCount(); i++) {
+#         storage.GetObject(i).show_parametri();
+#     }
+#     system("pause");
+#     return 0;
+    
+# } #endregion
